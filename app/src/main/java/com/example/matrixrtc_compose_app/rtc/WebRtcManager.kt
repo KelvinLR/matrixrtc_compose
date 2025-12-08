469df8873c198e6a56e2da66122df38b53e841e9
Kelvin <kelvindelima710@gmail.com>
2025-12-08 14:47:07 -0300

    feat: import create testWebRtc method

--- app/src/main/java/com/example/matrixrtc_compose_app/rtc/WebRtcManager.kt
+++ app/src/main/java/com/example/matrixrtc_compose_app/rtc/WebRtcManager.kt
@@ -1,13 +1,28 @@
 import android.content.Context
+import android.util.Log
 import com.example.matrixrtc_compose_app.rtc.WebRtcInitializer
 import org.webrtc.PeerConnectionFactory
 
 class WebRtcManager (private val context: Context) {
+    private val factory: PeerConnectionFactory by lazy {
+        PeerConnectionFactory.builder().createPeerConnectionFactory()
+    }
+
     init {
         WebRtcInitializer.initialize(context)
     }
 
-    private val factory: PeerConnectionFactory by lazy {
-        PeerConnectionFactory.builder().createPeerConnectionFactory()
+    fun testWebRtc () {
+        Log.i("RTC", "iniciando WebRTC...")
+
+        WebRtcInitializer.initialize(context)
+
+        val factory = PeerConnectionFactory.builder().createPeerConnectionFactory()
+
+        Log.i("RTC", "Factory criado: $factory")
     }
 }
+
+
+
+
